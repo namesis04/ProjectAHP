c3f525e162b40887364382cac66482480dc174aa
namesis04 <nuel.naibaho36@gmail.com>
2021-04-26 11:59:31 +0700

    base

---
+++ src/Alternatif.java
@@ -0,0 +1,14 @@
+public class Alternatif {
+    String nim;
+    String nama;
+    Kriteria[] kriteria;
+    SubKriteria[] subkriteria;
+    float hasilAhir = 0f;
+    //Alternatif K1
+    public Alternatif(String nim, String nama, Kriteria[] kriteria, SubKriteria[] subkriteria){
+        this.nim = nim;
+        this.nama = nama;
+        this.kriteria = kriteria;
+        this.subkriteria = subkriteria;
+    }
+}
